dba24b0498e214b8979b8a4dfdbfd30048ed83ea
AlexAlf <alexallafi.dev@gmail.com>
2024-07-06 21:47:41 +0300

    Integrates AndroidRoomConventionPlugin

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -34,5 +34,9 @@ gradlePlugin {
             id = "runique.android.feature.ui"
             implementationClass = "AndroidFeatureUIConventionPlugin"
         }
+        register("androidRoom") {
+            id = "runique.android.room"
+            implementationClass = "AndroidRoomConventionPlugin"
+        }
     }
 }
