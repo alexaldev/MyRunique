f6a845068a826f5333b8f7b319888ed70f3998aa
AlexAlf <alexallafi.dev@gmail.com>
2024-07-24 00:04:24 +0300

    Implement IntroScreen composable

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,6 +19,9 @@ dependencyResolutionManagement {
         mavenCentral()
     }
 }
+
+gradle.startParameter.excludedTaskNames.addAll(listOf(":build-logic:convention:testClasses"))
+
 rootProject.name = "MyRunique"
 
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
