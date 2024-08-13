04b3e3409e59bb3e2ade70c4c41ba9b7851b8f3f
AlexAlf <alexallafi.dev@gmail.com>
2024-08-13 21:55:11 +0300

    Show pause dialog

--- run/presentation/src/main/java/com/alexallafi/run/presentation/active_run/ActiveRunScreen.kt
+++ run/presentation/src/main/java/com/alexallafi/run/presentation/active_run/ActiveRunScreen.kt
@@ -26,6 +26,7 @@ import com.alexallafi.presentation.designsystem.MyRuniqueTheme
 import android.Manifest
 import com.alexallafi.presentation.designsystem.StartIcon
 import com.alexallafi.presentation.designsystem.StopIcon
+import com.alexallafi.presentation.designsystem.components.RuniqueActionButton
 import com.alexallafi.presentation.designsystem.components.RuniqueDialog
 import com.alexallafi.presentation.designsystem.components.RuniqueFloatingActionButton
 import com.alexallafi.presentation.designsystem.components.RuniqueOutlinedActionButton
@@ -160,6 +161,36 @@ private fun ActiveRunScreen(
                     .fillMaxWidth()
             )
         }
+    }
+
+    if (!state.shouldTrack && state.hasStartedRunning) {
+        RuniqueDialog(
+            title = stringResource(id = R.string.running_is_paused),
+            onDismiss = {
+                onAction(ActiveRunAction.OnResumeRunClick)
+            },
+            description = stringResource(id = R.string.resume_or_finish_run),
+            primaryButton = {
+                RuniqueActionButton(
+                    text = stringResource(id = R.string.resume),
+                    isLoading = false,
+                    onClick = {
+                        onAction(ActiveRunAction.OnResumeRunClick)
+                    },
+                    modifier = Modifier.weight(1f)
+                )
+            },
+            secondaryButton = {
+                RuniqueOutlinedActionButton(
+                    text = stringResource(id = R.string.finish),
+                    isLoading = state.isSavingRun,
+                    onClick = {
+                        onAction(ActiveRunAction.OnFinishRunClick)
+                    },
+                    modifier = Modifier.weight(1f)
+                )
+            }
+        )
     }
 
     if (state.showLocationRationale || state.showNotificationRationale) {
