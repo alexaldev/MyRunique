ee20b482e7fd1a55b990573d7ea74058f66a2dc1
AlexAlf <alexallafi.dev@gmail.com>
2024-08-21 20:23:58 +0300

    Integrate Junit5 dependencies, add ConventionPlugin for that

---
+++ auth/domain/src/test/kotlin/com/alexallafi/auth/domain/UserDataValidatorTests.kt
@@ -0,0 +1,13 @@
+package com.alexallafi.auth.domain
+
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import org.junit.jupiter.api.Test
+
+class UserDataValidatorTests {
+
+    @Test
+    fun foo() {
+        assertThat(2 + 2).isEqualTo(4)
+    }
+}
